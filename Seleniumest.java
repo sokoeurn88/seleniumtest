c246500cde298eec112f445d1bceceea1e7b1cbf
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-01-31 10:52:06 -0600

    test 3 commit

--- Seleniumest.java
+++ Seleniumest.java
@@ -1,11 +1,15 @@
 package seleniumtest;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Seleniumest {
 	
 	public static void main(String args[]) {
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sokoeurn chhay\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
@@ -19,6 +23,9 @@ public class Seleniumest {
 			System.out.println("Fail");
 		}
 		
+		WebElement element = driver.findElement(By.id("APjFqb"));
+		element.sendKeys("Yahoo");
+		element.sendKeys(Keys.ENTER);
 		
 		driver.close();
 	}
